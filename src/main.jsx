c59c8edb72b5d32d8d205d3ba4e92fc3eb469c3f
Oleksandr <125139066+oleksamor@users.noreply.github.com>
2024-08-29 12:09:31 +0300

    connect to app, change store, main

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import "modern-normalize";
 import App from "./componenets/App";
 import "./index.css";
 import { Provider } from "react-redux";
-import { ReactDOM } from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import { store } from "./redux/store";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <App />
   </Provider>
